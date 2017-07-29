3638ab1ba4c5178dc5445d8fcf6308def9fb9272
Hack The Life <physics.man@outlook.com>
2017-07-29 11:35:25 +0200

    Update pythontest.py

--- pythontest.py
+++ pythontest.py
@@ -4,9 +4,22 @@ def fonction(*liste,**dictionnaire):
     print("Les paramètres non nommées sont : {}, et les paramètres nommées sont : {}".format(liste,dictionnaire))
 
 class Personne:
+    #Classe
     def __init__(self,first_name,name):
-        self.first_name = first_name
-        self.name = name
+        self._first_name = first_name
+        self._name = name
+    def _get_name(self):
+        return self.name
+    def _set_name(self,new_name):
+        self.name = new_name
+        return self.name
+    name = property(_get_name,_set_name)
+    def _get_first_name(self):
+        return self.first_name
+    def _set_first_name(self,new_first_name):
+        self.first_name = new_first_name
+        return self.first_name
+    first_name = property(_get_first_name,_set_first_name)
     def __del__(self):
         print("Vous avez supprimé votre objet")
     def __repr__(self):
@@ -23,6 +36,7 @@ class Zdict:
         self.name = name
 
 class Duree:
+    #Classe permettant d'afficher des minutes et des secondes
     def __init__(self,minutes,secondes):
         self.minutes = minutes
         self.secondes = secondes
@@ -34,6 +48,10 @@ class Compteur:
     def __init__(self,name):
         Compteur.compteur_objet += 1
         self.name = name
+
+class Temp:
+    #Classe avec des attributs temporaires
+    def __init__(self):
 
 
 fonction(1,2,3,4,5,a=6,b=7,c=8,d=9,e=10)
